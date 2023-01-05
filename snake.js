dae7c6953c8f885a27b62fa2fc7444253cffb01a
Madi <madiishka3@gmail.com>
2023-01-05 17:38:28 +0600

    MEGA HOT FIX

--- snake.js
+++ snake.js
@@ -1,22 +1,29 @@
+//Создание основных переменных
+
 let dir;
-let block = 10;
 let snakeY = 50;
 let snakeX = 50;
+
+
+//Определение изначальных координат змеи
 
 document.getElementById("snake2").style.top = snakeY + "px";
 document.getElementById("snake2").style.left = snakeX + "px";
 
+//Изменение направления
+
 document.addEventListener("keydown", function(event){
 	if (event.code == "KeyW"){
 		dir = "up";
-	} else if (event.code = "KeyD"){
+	} else if (event.code == "KeyD"){
 		dir = "right";
-	} else if (event.code = "KeyA"){
+	} else if (event.code == "KeyA"){
 		dir = "left";
-	} else if (event.code = "KeyS"){
+	} else if (event.code == "KeyS"){
 		dir = "down";
 	}
 })
+//Движение
 
 let snake = document.getElementById("snake2")
 
@@ -24,21 +31,15 @@ function movement(){
 	if (dir == "up"){
 		snakeY = snakeY - 10;
 		snake.style.top = snakeY + "px";
-	}
-	if (dir == "down"){
+	}else if (dir == "down"){
 		snakeY = snakeY + 10
 		snake.style.top = snakeY + "px";
-	}
-	if (dir == "right"){
+	}else if (dir == "right"){
 		snakeX = snakeX + 10;
 		snake.style.left = snakeX + "px";
-	}
-	if ( dir = "left") {
+	} else if ( dir = "left") {
 		snakeX = snakeX - 10;
 		snake.style.left = snakeX + "px";
-	} else {
-		snakeX = 50;
-		snakeY = 50;
 	}
 }
 
