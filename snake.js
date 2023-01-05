3c47451412261d7389927016a6e1e3a4e6591a18
Madi <madiishka3@gmail.com>
2023-01-05 17:01:26 +0600

    movement fix

--- snake.js
+++ snake.js
@@ -1,2 +1,23 @@
 let dir;
 let block = 10;
+let snake = document.getElementById('snake2');
+
+
+document.addEventListener("keydown", function(event){
+	if (event.code == "KeyW"){
+		dir = "up";
+	} else if (event.code = "KeyD"){
+		dir = "right";
+	} else if (event.code = "KeyA"){
+		dir = "left";
+	} else if (event.code = "KeyS"){
+		dir = "down";
+	}
+})
+function movement(){
+	if (dir =="up"){
+		snake.style.top += 10
+	}
+}
+
+let game = setInterval(movement, 100)
